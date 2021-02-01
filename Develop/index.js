7693672037f085e443b38ab43bea781eb7e74ed0
Chris Curry <Chamilitary216@hotmail.com>
2021-01-31 21:08:42 -0500

    created an array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,64 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
+//description, installation instructions, usage information, contribution guidelines, and test instructions
 const questions = [];
+    {
+        type: "input",
+        name: "projectName",
+        message: "What is the name of the project?",
+        
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Write a decription of your project please",
+    },
+
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install this project?",
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the use of this project?",
+    },
+
+    {
+        type: "list",
+        name: "contribution",
+        message: "Who contributed to this project?",
+    }
+
+    {
+        type: "input",
+        name: "test",
+        message: "Is there a test included?",
+    },
+
+    {
+        type: "list",
+        name: "license"
+        message: "Which licenses are used for this project?",
+        choices: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        ]
+    }
+
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
